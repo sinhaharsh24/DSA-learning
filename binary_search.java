c415acf11788af474d710a09befdf74f93b43316
harsh <sinhaharsh909@gmail.com>
2025-02-03 22:48:04 +0530

    same changes

--- binary_search.java
+++ binary_search.java
@@ -1,5 +1,10 @@
 public class binary_search {
     public static void main(String[] args) {
-        
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        int target = 9;
+
+        for (int i = 0; i < arr.length; i++) {
+            
+        }
     }
 }
