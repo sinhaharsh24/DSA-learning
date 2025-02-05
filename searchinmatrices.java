ab716c8362aec5882efbdaa5563b459ae54b960c
harsh <sinhaharsh909@gmail.com>
2025-02-05 23:34:24 +0530

    new files added

---
+++ searchinmatrices.java
@@ -0,0 +1,8 @@
+public class searchinmatrices {
+    public static void main(String[] args) {
+        int[][] arr = { {1,2,3,4} ,
+                {5,6,7,8} , };
+        int target = 8;
+
+    }
+}
