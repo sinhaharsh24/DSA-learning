2ce6b3c27786bc92d9e66edcdedb82a1d1915c9b
harsh <sinhaharsh909@gmail.com>
2025-02-09 23:41:36 +0530

    some changes

---
+++ insertion_sort.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class insertion_sort {
+    public static void main(String[] args) {
+        int[] arr = {3, 6, 2, 7, 4, 5};
+        int[] ans = sorting(arr);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    static int[] sorting(int[] arr) {
+        
+    }
+}
