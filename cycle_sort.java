f05f339380fa7fd0b6816f7a9e5c9d70fd07ffe2
harsh <sinhaharsh909@gmail.com>
2025-02-11 23:07:38 +0530

    some changes

---
+++ cycle_sort.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class cycle_sort {
+    public static void main(String[] args) {
+        int[] arr = {3, 6, 2, 7, 4, 5};
+        int ans = sorting(arr);
+        System.out.println(Arrays.toString(ans));
+    }
+    static int sorting(int[] arr) {
+        
+    }
+}
