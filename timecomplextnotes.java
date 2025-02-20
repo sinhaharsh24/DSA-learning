8b6c89bdc8978c7fb5f14c99e7a7e3aaae0327ef
harsh <sinhaharsh909@gmail.com>
2025-02-20 23:18:46 +0530

    some changes

--- timecomplextnotes.java
+++ timecomplextnotes.java
@@ -1,6 +1,8 @@
 public class timecomplextnotes {
     public static void main(String[] args) {
         //time complexity and time used by computer to done the computation is defferent
-        
+        //the comparision is like
+        // O(1) < O(log N) < O(N) < O(N log N) < O(2^n)
+
     }
 }
