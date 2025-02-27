4d5f0ca555fdaae8fd2b8111a5e4ce20f1499328
harsh <sinhaharsh909@gmail.com>
2025-02-27 23:35:48 +0530

    some new topics covered

--- reverseanumberusingrecursion.java
+++ reverseanumberusingrecursion.java
@@ -1,13 +1,24 @@
 public class reverseanumberusingrecursion {
+    static int sum = 0;
+    static void reverse(int n){
+        if(n == 0) {
+            return;
+        }
+        int rem = n % 10;
+        sum = sum * 10 + rem;
+        reverse(n/10);
+    }
     public static void main(String[] args) {
-        int answer = reverse(12345); //return should be 54321
-        System.out.println(answer);
+        reverse(12345);
+        System.out.println(sum);
 
     }
-    static int reverse(int n){
-        if(n%10 == n) return n;
-        int digit = (n % 10) * (int) Math.pow(10, (int) Math.log10(n)) + reverse(n / 10);
-        return digit;
-    }
+    // static int reverse(int n){
+    //     if(n%10 == n) return n;
+    //     int digit = (n % 10) * (int) Math.pow(10, (int) Math.log10(n)) + reverse(n / 10);
+    //     return digit;
+    // }
+
+    
     
 }
